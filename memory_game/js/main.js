044b876a631e22ae4a48af7fd1115c776fbb7787
John William Davis <jwdavisdeveloper@gmail.com>
2019-12-01 14:16:03 -0800

    created some game logic with js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,24 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
+// console.log("User flipped " + cardTwo)
+// console.log("User flipped " + cardFour)
+
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [];
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+cardTwo = cards[2]
+cardsInPlay.push(cardTwo)
 console.log("User flipped " + cardTwo)
-console.log("User flipped " + cardFour)
+
+if (cardsInPlay.length === 2) {
+    if (cardOne === cardTwo) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    }
+}
