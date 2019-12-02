6939c10f19400772efdbe49857830e3b70a89ac4
John William Davis <jwdavisdeveloper@gmail.com>
2019-12-02 14:29:14 -0800

    Continue work on game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,19 +6,23 @@
 // console.log("User flipped " + cardTwo)
 // console.log("User flipped " + cardFour)
 
-cards = ["queen", "queen", "king", "king"];
-cardsInPlay = [];
-cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardTwo = cards[2]
-cardsInPlay.push(cardTwo)
-console.log("User flipped " + cardTwo)
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-if (cardsInPlay.length === 2) {
-    if (cardOne === cardTwo) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again.");
+function checkForMatch(){
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+}
+function flipCard(cardId){
+    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId]);
+    if (cardsInPlay.length === 2) {
+        checkForMatch();
     }
 }
+
+flipCard(0);
+flipCard(2);
