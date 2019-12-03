6471ea1c7e7fb297e8e9919f3f4bf18a2a069bc1
John William Davis <jwdavisdeveloper@gmail.com>
2019-12-02 23:29:36 -0800

    Working on the javascript

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,44 @@
-// var cardOne = "queen";
-// var cardTwo = "queen";
-// var cardThree = "king";
-// var cardFour = "king";
 
-// console.log("User flipped " + cardTwo)
-// console.log("User flipped " + cardFour)
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+
+},
+{
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+},
+{
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+}
+];
+
+
 var cardsInPlay = [];
 
 function checkForMatch(){
     if (cardsInPlay[0] === cardsInPlay[1]) {
-        console.log("You found a match!");
+        alert("You found a match!");
       } else {
-        console.log("Sorry, try again.");
+        alert("Sorry, try again.");
       }
 }
 function flipCard(cardId){
-    cardsInPlay.push(cards[cardId]);
-    console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].cardImage)
+    console.log(cards[cardId].suit)
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
