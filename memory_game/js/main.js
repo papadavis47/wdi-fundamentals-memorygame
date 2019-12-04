d693ba978a14f65af80d552880705622109bd007
John William Davis <jwdavisdeveloper@gmail.com>
2019-12-03 22:47:03 -0800

    created game logic - end of exercise

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,15 @@ var cards = [
 }
 ];
 
+function createBoard() {
+  for (let i = 0; i < cards.length; i++) {
+    const cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
 var cardsInPlay = [];
 
@@ -34,15 +43,16 @@ function checkForMatch(){
         alert("Sorry, try again.");
       }
 }
-function flipCard(cardId){
+function flipCard(){
+    const cardId = this.getAttribute('data-id');
     cardsInPlay.push(cards[cardId].rank);
     console.log("User flipped " + cards[cardId].rank);
-    console.log(cards[cardId].cardImage)
-    console.log(cards[cardId].suit)
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
